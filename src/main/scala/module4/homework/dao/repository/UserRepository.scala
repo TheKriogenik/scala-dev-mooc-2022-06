ea164439683380868475ff328ff5feaf87ae32a8
Kriogenik <thekriogenik@gmail.com>
2022-11-14 23:33:37 +0300

    Add implementations

--- src/main/scala/module4/homework/dao/repository/UserRepository.scala
+++ src/main/scala/module4/homework/dao/repository/UserRepository.scala
@@ -1,22 +1,15 @@
 package module4.homework.dao.repository
 
-import zio.{Has, ULayer, ZIO, ZLayer}
 import io.getquill.context.ZioJdbc._
-import module4.homework.dao.entity.User
-import zio.macros.accessible
-import module4.homework.dao.entity.{Role, UserToRole}
-import module4.homework.dao.entity.UserId
-import module4.homework.dao.entity.RoleCode
+import module4.homework.dao.entity._
 import module4.phoneBook.db
-
-import java.sql.SQLException
-import javax.sql.DataSource
+import zio.{Has, ULayer, ZLayer}
 
 
 object UserRepository{
 
 
-    val dc = db.Ctx
+    val dc: db.Ctx.type = db.Ctx
     import dc._
 
     type UserRepository = Has[Service]
@@ -35,41 +28,71 @@ object UserRepository{
         def findRoleByCode(roleCode: RoleCode): QIO[Option[Role]]
     }
 
-    class ServiceImpl extends Service{
+    private class ServiceImpl extends Service{
 
-        lazy val userSchema = quote {
+        lazy private val userSchema = quote {
             querySchema[User](""""User"""")
         }
 
+        lazy private val roleSchema = quote {
+            querySchema[Role](""""Role"""")
+        }
 
-        lazy val roleSchema = ???
+        lazy private val userToRoleSchema = quote {
+            querySchema[UserToRole](""""UserToRole"""")
+        }
 
-        lazy val userToRoleSchema = ???
-
-        def findUser(userId: UserId): Result[Option[User]] = run(userSchema.filter(_.id == lift(userId.id)))
-          .map(_.headOption)
+        def findUser(userId: UserId): Result[Option[User]] = run(
+            userSchema.filter(_.id == lift(userId.id))
+        ).map(_.headOption)
         
         def createUser(user: User): Result[User] = run(userSchema.insert(lift(user))).as(user)
         
-        def createUsers(users: List[User]): Result[List[User]] = ???
+        def createUsers(users: List[User]): Result[List[User]] = run(
+            liftQuery(users).foreach(u => userSchema.insert(u).returning(x => x))
+        )
         
-        def updateUser(user: User): Result[Unit] = ???
+        def updateUser(user: User): Result[Unit] = run(
+            userSchema.filter(x => x.id == lift(user.id)).update(
+                _.lastName  -> lift(user.lastName),
+                _.firstName -> lift(user.firstName),
+                _.age       -> lift(user.age))
+        ).unit
         
-        def deleteUser(user: User): Result[Unit] = ???
+        def deleteUser(user: User): Result[Unit] = run(
+            userSchema.filter(_.id == lift(user.id)).delete
+        ).unit
         
-        def findByLastName(lastName: String): Result[List[User]] = ???
+        def findByLastName(lastName: String): Result[List[User]] = run (
+            userSchema.filter(_.lastName == lift(lastName))
+        )
         
-        def list(): Result[List[User]] = ???
+        def list(): Result[List[User]] = run(userSchema)
         
-        def userRoles(userId: UserId): Result[List[Role]] = ???
+        def userRoles(userId: UserId): Result[List[Role]] = run(
+            for {
+                userToRole <- userToRoleSchema.filter(_.userId == lift(userId.id))
+                role <- roleSchema.join(_.code == userToRole.roleId)
+            } yield role
+        )
         
-        def insertRoleToUser(roleCode: RoleCode, userId: UserId): Result[Unit] = ???
+        def insertRoleToUser(roleCode: RoleCode, userId: UserId): Result[Unit] = run(
+            userToRoleSchema.insert(UserToRole(lift(roleCode.code), lift(userId.id)))
+        ).unit
+
         
-        def listUsersWithRole(roleCode: RoleCode): Result[List[User]] = ???
+        def listUsersWithRole(roleCode: RoleCode): Result[List[User]] = run (
+            for {
+                userToRole <- userToRoleSchema.filter(_.roleId == lift(roleCode.code))
+                user <- userSchema.join(_.id == userToRole.userId)
+            } yield user
+        )
         
-        def findRoleByCode(roleCode: RoleCode): Result[Option[Role]] = ???
+        def findRoleByCode(roleCode: RoleCode): Result[Option[Role]] = run (
+            roleSchema.filter(_.code == lift(roleCode.code))
+        ).map(_.headOption)
                 
     }
 
-    val live: ULayer[UserRepository] = ???
+    val live: ULayer[UserRepository] = ZLayer.succeed(new ServiceImpl)
 }
--- src/main/scala/module4/homework/services/UserService.scala
+++ src/main/scala/module4/homework/services/UserService.scala
@@ -1,47 +1,56 @@
 package module4.homework.services
 
-import zio.Has
-import zio.Task
-import module4.homework.dao.entity.User
-import module4.homework.dao.entity.Role
+import module4.homework.dao.entity.{Role, RoleCode, User}
 import module4.homework.dao.repository.UserRepository
-import zio.ZIO
-import zio.RIO
-import module4.homework.dao.entity.UserToRole
-import zio.ZLayer
-import zio.macros.accessible
-import module4.homework.dao.entity.RoleCode
 import module4.phoneBook.db
+import zio.macros.accessible
+import zio.{Has, RIO, ZIO, ZLayer}
 
 @accessible
-object UserService{
-    type UserService = Has[Service]
+object UserService {
+  type UserService = Has[Service]
 
-    trait Service{
-        def listUsers(): RIO[db.DataSource, List[User]]
-        def listUsersDTO(): RIO[db.DataSource, List[UserDTO]]
-        def addUserWithRole(user: User, roleCode: RoleCode): RIO[db.DataSource, UserDTO]
-        def listUsersWithRole(roleCode: RoleCode): RIO[db.DataSource, List[UserDTO]]
-    }
+  trait Service {
+    def listUsers(): RIO[db.DataSource, List[User]]
 
-    class Impl(userRepo: UserRepository.Service) extends Service{
-        val dc = db.Ctx
-        import dc._
+    def listUsersDTO(): RIO[db.DataSource, List[UserDTO]]
 
-        def listUsers(): RIO[db.DataSource, List[User]] =
-        userRepo.list()
+    def addUserWithRole(user: User, roleCode: RoleCode): RIO[db.DataSource, UserDTO]
+
+    def listUsersWithRole(roleCode: RoleCode): RIO[db.DataSource, List[UserDTO]]
+  }
+
+  private case class Impl(userRepo: UserRepository.Service) extends Service {
+    private val dc = db.Ctx
+
+    def listUsers(): RIO[db.DataSource, List[User]] = userRepo.list()
 
 
-        def listUsersDTO(): RIO[db.DataSource,List[UserDTO]] = ???
-        
-        def addUserWithRole(user: User, roleCode: RoleCode): RIO[db.DataSource, UserDTO] = ???
-        
-        def listUsersWithRole(roleCode: RoleCode): RIO[db.DataSource,List[UserDTO]] = ???
-        
-        
-    }
+    def listUsersDTO(): RIO[db.DataSource, List[UserDTO]] = for {
+      users <- listUsers()
+      result <- ZIO.foreach(users) { user =>
+        userRepo.userRoles(user.typedId).map(x => UserDTO(user, x.toSet))
+      }
+    } yield result
 
-    val live: ZLayer[UserRepository.UserRepository, Nothing, UserService] = ???
+    def addUserWithRole(user: User, roleCode: RoleCode): RIO[db.DataSource, UserDTO] = dc.transaction(
+      for {
+        user <- userRepo.createUser(user)
+        _ <- userRepo.insertRoleToUser(roleCode, user.typedId)
+        roles <- userRepo.userRoles(user.typedId)
+      } yield UserDTO(user, roles.toSet))
+
+    def listUsersWithRole(roleCode: RoleCode): RIO[db.DataSource, List[UserDTO]] = for {
+      users <- userRepo.listUsersWithRole(roleCode)
+      result <- ZIO.foreach(users) { user =>
+        userRepo.userRoles(user.typedId).map(x => UserDTO(user, x.toSet))
+      }
+    } yield result
+
+
+  }
+
+  val live: ZLayer[UserRepository.UserRepository, Nothing, UserService] = ZLayer.fromService(Impl.apply)
 }
 
 case class UserDTO(user: User, roles: Set[Role])
